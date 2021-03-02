e29b98fdfaab84ebecb5f7c3bc9c1f294c0b7057
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-02 13:23:53 +0300

    + Output ex. 1.1 ! Refactored ex. 1.1

--- Week06/Exercise 1.1.sql
+++ Week06/Exercise 1.1.sql
@@ -1,11 +1,17 @@
-PREPARE exercise1 AS SELECT title
-                     FROM film,
-                          film_category
-                     WHERE (rating = 'R' OR rating = 'PG-13')
-                       AND (film.film_id = film_category.film_id AND
-                            film_category.category_id IN
-                            (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
+PREPARE exercise1 AS
+    SELECT title
+    FROM film,
+         film_category
+    WHERE film.film_id NOT IN
+          (SELECT DISTINCT film_id
+           FROM rental JOIN inventory ON rental.inventory_id = inventory.inventory_id)
+      AND (rating = 'R' OR rating = 'PG-13')
+      AND (film.film_id = film_category.film_id AND
+           film_category.category_id IN
+           (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
 
 EXPLAIN ANALYZE EXECUTE exercise1;
+EXECUTE exercise1;
 
 DEALLOCATE exercise1;
+
