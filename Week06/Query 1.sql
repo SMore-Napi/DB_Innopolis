b4bfa053951ac5c9187719be3806889b35b97c4f
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-02 17:33:21 +0300

    + Output ex. 1.2 ! Refactored ex. 1.2

--- Week06/Query 1.sql
+++ Week06/Query 1.sql
@@ -20,25 +20,20 @@ DEALLOCATE exercise1;
 
 -- Exercise 2.
 
-CREATE INDEX inventory_idx ON inventory (inventory_id);
-CREATE INDEX rental_idx ON rental (inventory_id);
-
-PREPARE exercise1 AS
+PREPARE exercise2 AS
     SELECT title
     FROM film,
          film_category
     WHERE film.film_id NOT IN
-          (SELECT DISTINCT film_id
-           FROM rental JOIN inventory ON rental.inventory_id = inventory.inventory_id)
+          (SELECT film_id
+           FROM inventory INNER JOIN rental ON rental.inventory_id = inventory.inventory_id)
       AND (rating = 'R' OR rating = 'PG-13')
       AND (film.film_id = film_category.film_id AND
            film_category.category_id IN
            (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
 
-EXPLAIN ANALYZE EXECUTE exercise1;
-EXPLAIN EXECUTE exercise1;
-EXECUTE exercise1;
+EXPLAIN ANALYZE EXECUTE exercise2;
+EXPLAIN EXECUTE exercise2;
+EXECUTE exercise2;
 
-DEALLOCATE exercise1;
-DROP INDEX inventory_idx;
-DROP INDEX rental_idx;
+DEALLOCATE exercise2;
