41f3cc6df16931954a44010729cf2c1bfba70c5d
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-02 17:23:59 +0300

    + Output ex. 1.2 ! Refactored ex. 1.2

--- Week06/Query 1.sql
+++ Week06/Query 1.sql
@@ -1,11 +1,44 @@
-PREPARE exercise1 AS SELECT title
-                     FROM film,
-                          film_category
-                     WHERE (rating = 'R' OR rating = 'PG-13')
-                       AND (film.film_id = film_category.film_id AND
-                            film_category.category_id IN
-                            (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
+-- Exercise 1.
+
+PREPARE exercise1 AS
+    SELECT title
+    FROM film,
+         film_category
+    WHERE film.film_id NOT IN
+          (SELECT DISTINCT film_id
+           FROM rental JOIN inventory ON rental.inventory_id = inventory.inventory_id)
+      AND (rating = 'R' OR rating = 'PG-13')
+      AND (film.film_id = film_category.film_id AND
+           film_category.category_id IN
+           (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
 
 EXPLAIN ANALYZE EXECUTE exercise1;
+EXPLAIN EXECUTE exercise1;
+EXECUTE exercise1;
 
 DEALLOCATE exercise1;
+
+-- Exercise 2.
+
+CREATE INDEX inventory_idx ON inventory (inventory_id);
+CREATE INDEX rental_idx ON rental (inventory_id);
+
+PREPARE exercise1 AS
+    SELECT title
+    FROM film,
+         film_category
+    WHERE film.film_id NOT IN
+          (SELECT DISTINCT film_id
+           FROM rental JOIN inventory ON rental.inventory_id = inventory.inventory_id)
+      AND (rating = 'R' OR rating = 'PG-13')
+      AND (film.film_id = film_category.film_id AND
+           film_category.category_id IN
+           (SELECT category_id FROM category WHERE name = 'Horror' OR name = 'Sci-Fi'));
+
+EXPLAIN ANALYZE EXECUTE exercise1;
+EXPLAIN EXECUTE exercise1;
+EXECUTE exercise1;
+
+DEALLOCATE exercise1;
+DROP INDEX inventory_idx;
+DROP INDEX rental_idx;
