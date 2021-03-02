47ee15837a0b49152f5e554acf6e2263169030a5
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-02 13:38:15 +0300

    + Output ex. 1.2 ! Refactored ex. 1.2

--- Week06/Exercise 1.2.sql
+++ Week06/Exercise 1.2.sql
@@ -1,9 +1,13 @@
-SELECT A.st_id as store_id, SUM (A.amount) AS amount, address.address as store_address FROM
-(SELECT payment.staff_id, amount, payment_date,
-       staff.store_id, staff.staff_id,
-       store.store_id as st_id, manager_staff_id, store.address_id as id
-FROM public.payment, public.staff, public.store
-WHERE payment.staff_id = staff.staff_id AND staff.store_id = store.store_id AND
-      payment.payment_date >='2007-05-01') AS A, address
-WHERE A.id=address.address_id
-GROUP BY A.st_id, store_address
+PREPARE exercise2 AS
+    SELECT A.st_id as store_id, SUM (A.amount) AS amount, address.address as store_address FROM
+    (SELECT payment.staff_id, amount, payment_date,
+           staff.store_id, staff.staff_id,
+           store.store_id as st_id, manager_staff_id, store.address_id as id
+    FROM public.payment, public.staff, public.store
+    WHERE payment.staff_id = staff.staff_id AND staff.store_id = store.store_id AND
+          payment.payment_date >='2007-05-01') AS A, address
+    WHERE A.id=address.address_id
+    GROUP BY A.st_id, store_address;
+
+EXPLAIN ANALYZE EXECUTE exercise2;
+EXECUTE exercise2;
