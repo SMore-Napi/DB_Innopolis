06a00e40ae77b0b6601e87ed06ae9edcfa823e71
SMore-Napi <s.roma245@gmail.com>
2021-03-09 15:02:47 +0300

    Week 7. Exercise 1.

--- Week07/Exercise 1. Query.sql
+++ Week07/Exercise 1. Query.sql
@@ -1,44 +1,23 @@
--- Exercise 2.
-CREATE OR REPLACE FUNCTION get_customers(start_id integer, end_id integer)
+-- Exercise 1.
+CREATE OR REPLACE FUNCTION get_addresses()
     RETURNS TABLE
             (
-                customer_id INT,
-                first_name  VARCHAR(45),
-                last_name   VARCHAR(45),
-                email       VARCHAR(50),
-                address_id  SMALLINT
+                address_id  integer,
+                address     varchar(50),
+                address2    varchar(50),
+                district    varchar(20),
+                city_id     smallint,
+                postal_code varchar(10),
+                phone       varchar(20),
+                last_update timestamp
             )
 AS
 $$
 BEGIN
-    IF start_id < 0 THEN
-        RAISE EXCEPTION 'Start address_id cannot be negative.';
-    END IF;
-    IF start_id > 600 THEN
-        RAISE EXCEPTION 'Start address_id cannot be greater than 600.';
-    END IF;
-    IF end_id < 0 THEN
-        RAISE EXCEPTION 'End address_id cannot be negative.';
-    END IF;
-    IF end_id > 600 THEN
-        RAISE EXCEPTION 'End address_id cannot be greater than 600.';
-    END IF;
-    IF start_id >= end_id THEN
-        RAISE EXCEPTION 'End address_id cannot be greater than start address_id.';
-    END IF;
     RETURN QUERY
-        SELECT customer.customer_id, customer.first_name, customer.last_name, customer.email, customer.address_id
-        FROM customer
-        WHERE start_id <= customer.address_id
-          AND customer.address_id < end_id
-        ORDER BY customer.address_id;
+        SELECT *
+        FROM address
+        WHERE address.city_id >= 400
+          AND address.city_id <= 600
+          AND address.address LIKE '%11%';
 END;
-$$ LANGUAGE plpgsql;
-
-SELECT * FROM  get_customers(10, 40);
-SELECT * FROM  get_customers(10, 11);
-SELECT * FROM  get_customers(0, 600);
-SELECT * FROM  get_customers(-10, 40);
-SELECT * FROM  get_customers(10, 601);
-SELECT * FROM  get_customers(10, -10);
-SELECT * FROM  get_customers(30, 10);
