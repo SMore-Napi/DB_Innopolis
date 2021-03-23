33b22a31fcac00d18190d2251fb23f74e90f5b26
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-23 12:02:19 +0300

    ! edited part 1

--- Week08/Exercise 1 (part 1).sql
+++ Week08/Exercise 1 (part 1).sql
@@ -17,40 +17,35 @@ VALUES ('Account 1', 1000),
        ('Account 3', 1000);
 
 
--- Transaction T1.
 BEGIN;
+SAVEPOINT ORIGIN;
+
+-- Transaction T1.
 UPDATE accounts
 SET credit = credit - 500
 WHERE name = 'Account 1';
 UPDATE accounts
 SET credit = credit + 500
 WHERE name = 'Account 3';
-COMMIT;
-
 
 -- Transaction T2.
-BEGIN;
 UPDATE accounts
 SET credit = credit - 700
 WHERE name = 'Account 2';
 UPDATE accounts
 SET credit = credit + 700
 WHERE name = 'Account 1';
-COMMIT;
-
 
 -- Transaction T3.
-BEGIN;
 UPDATE accounts
 SET credit = credit - 100
 WHERE name = 'Account 2';
 UPDATE accounts
 SET credit = credit + 100
 WHERE name = 'Account 3';
-COMMIT;
-
 
 -- Return credits for all accounts.
 SELECT credit
 FROM accounts;
 
+ROLLBACK TO ORIGIN;
