6a9571ed9fc0cab39be3c5f0764436a5e054d1f6
Danis Alukaev <d.alukaev@innopolis.university>
2021-03-23 12:30:53 +0300

    ! part 2

--- Week08/Exercise 1.sql
+++ Week08/Exercise 1.sql
@@ -1,6 +1,6 @@
--- Exercise 1
--- Part 1
+-- Exercise 1.
 
+-- Part 1.
 -- Create accounts table.
 CREATE TABLE accounts
 (
@@ -15,7 +15,7 @@ VALUES ('Account 1', 1000),
        ('Account 2', 1000),
        ('Account 3', 1000);
 
--- Shared transaction.
+-- Shared transaction for part 1.
 BEGIN;
 SAVEPOINT ORIGIN;
 
@@ -51,16 +51,54 @@ FROM accounts;
 ROLLBACK TO ORIGIN;
 
 
--- Exercise 1
--- Part 2
-
+-- Part 2.
 -- Create new attribute bank_name.
 ALTER TABLE accounts
-    ADD bank_name VARCHAR(20) DEFAULT NULL;
+ADD bank_name VARCHAR(20) DEFAULT NULL;
+UPDATE accounts SET bank_name = 'SpearBank' WHERE name = 'Account 1' or name = 'Account 3';
+UPDATE accounts SET bank_name = 'Tinkoff' WHERE name = 'Account 2';
+
+-- Create new account to store fees.
+INSERT INTO accounts (name, credit, bank_name)
+VALUES ('Account 4', 0, NULL);
+
+-- Shared transaction for part 2.
+BEGIN;
+SAVEPOINT ORIGIN;
+
+-- Transaction T1.
 UPDATE accounts
-SET bank_name = 'SpearBank'
-WHERE name = 'Account 1'
-   or name = 'Account 3';
+SET credit = credit - 500
+WHERE name = 'Account 1';
 UPDATE accounts
-SET bank_name = 'Tinkoff'
+SET credit = credit + 500
+WHERE name = 'Account 3';
+
+-- Transaction T2.
+UPDATE accounts
+SET credit = credit - 730
 WHERE name = 'Account 2';
+UPDATE accounts
+SET credit = credit + 700
+WHERE name = 'Account 1';
+UPDATE accounts
+SET credit = credit + 30
+WHERE name = 'Account 4';
+
+-- Transaction T3.
+UPDATE accounts
+SET credit = credit - 130
+WHERE name = 'Account 2';
+UPDATE accounts
+SET credit = credit + 100
+WHERE name = 'Account 3';
+UPDATE accounts
+SET credit = credit + 30
+WHERE name = 'Account 4';
+
+-- Return credits for all accounts.
+SELECT credit
+FROM accounts;
+
+-- Return to the original state.
+ROLLBACK TO ORIGIN;
