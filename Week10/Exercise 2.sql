9932e9664e723ca33d0ba57a09ffd5052c45398b
Danis Alukaev <d.alukaev@innopolis.university>
2021-04-06 11:49:53 +0300

    ! ex.2

--- Week10/Exercise 2.sql
+++ Week10/Exercise 2.sql
@@ -1,35 +1,33 @@
 -- TERMINAL 1.
 
--- Step 1.
+-- Step 1. Start a transaction.
 BEGIN;
 
--- Step 3.
-SELECT *
-FROM account
-WHERE group_id = 2;
+-- Set isolation level.
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
--- Step 5.
-SELECT *
-FROM account
-WHERE group_id = 2;
+-- Step 3. Read accounts with group_id=2.
+SELECT * FROM account WHERE group_id = 2;
 
--- Step 6.
-UPDATE account
-SET balance = balance + 15
-WHERE group_id = 2;
+-- Step 5. Read accounts with group_id=2.
+SELECT * FROM account WHERE group_id = 2;
 
--- Step 7.
+-- Step 6. Update selected accounts balances by +15.
+UPDATE account SET balance = balance + 15 WHERE group_id = 2;
+
+-- Step 7. Commit transaction.
 COMMIT;
 
 -- TERMINAL 2.
 
--- Step 2.
+-- Step 2. Start a transaction.
 BEGIN;
 
--- Step 4.
-UPDATE account
-SET group_id = 2
-WHERE fullname = 'Bob Brown';
+-- Set isolation level.
+SET TRANSACTION ISOLATION LEVEL READ COMMITTED;
 
--- Step 8.
+-- Step 4. Move Bob to group 2.
+UPDATE account SET group_id = 2 WHERE fullname = 'Bob Brown';
+
+-- Step 8. Commit transaction.
 COMMIT;
